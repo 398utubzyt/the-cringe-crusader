2735685ac6fee4b3de3de311228fec05fb96b5ab
398utubzyt <398utubzyt@gmail.com>
2023-04-05 01:03:25 -0700

    Ensure no repetition

--- src/Tod/TruthOrDare.cs
+++ src/Tod/TruthOrDare.cs
@@ -10,6 +10,10 @@ namespace Crusader.Tod
         private readonly FileDatabase<TodPrompt> dares;
         private readonly FileDatabase<TodPrompt> wyrs;
 
+        private int lastTruth;
+        private int lastDare;
+        private int lastWyr;
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static TodPrompt ParsePromptTruth(string text) => new TodPrompt() { Type = TodType.Truth, Text = text };
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -19,20 +23,26 @@ namespace Crusader.Tod
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static string ConvertPrompt(TodPrompt prompt) => prompt.Text;
 
+        /// <summary>Returns an index that will never equal the parameter <paramref name="prev"/>.</summary>
+        /// <returns>The index which never equals <paramref name="prev"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int EnsureDifferent(double random, int count, int prev)
+            => prev >= 0 ? ((count = (int)(random * (count - 1))) < prev ? count : count + 1) : (int)(random * count);
+
         /// <summary>Gets a random Truth prompt.</summary>
         /// <returns>A random truth prompt.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TodPrompt GetTruth() => truths[(int)(Random.Shared.NextDouble() * truths.Count)];
+        public TodPrompt GetTruth() => truths[lastTruth = EnsureDifferent(Random.Shared.NextDouble(), truths.Count, lastTruth)];
 
         /// <summary>Gets a random Dare prompt.</summary>
         /// <returns>A random dare prompt.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TodPrompt GetDare() => dares[(int)(Random.Shared.NextDouble() * dares.Count)];
+        public TodPrompt GetDare() => dares[lastDare = EnsureDifferent(Random.Shared.NextDouble(), dares.Count, lastDare)];
 
         /// <summary>Gets a random Would You Rather prompt.</summary>
         /// <returns>A random Would You Rather prompt.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TodPrompt GetWyr() => wyrs[(int)(Random.Shared.NextDouble() * wyrs.Count)];
+        public TodPrompt GetWyr() => wyrs[lastWyr = EnsureDifferent(Random.Shared.NextDouble(), wyrs.Count, lastWyr)];
 
         /// <summary>Gets a random Truth or Dare prompt.</summary>
         /// <returns>A random prompt.</returns>
@@ -76,6 +86,10 @@ namespace Crusader.Tod
             truths = new FileDatabase<TodPrompt>(FileUtil.Root("truth.txt"), ParsePromptTruth);
             dares = new FileDatabase<TodPrompt>(FileUtil.Root("dare.txt"), ParsePromptDare);
             wyrs = new FileDatabase<TodPrompt>(FileUtil.Root("wyr.txt"), ParsePromptWyr);
+
+            lastTruth = -1;
+            lastDare = -1;
+            lastWyr = -1;
         }
     }
 }
